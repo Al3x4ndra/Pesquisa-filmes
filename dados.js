ec359e3b8802c538ff226c4d764a8d8034d3265b
Alexandra <107573513+Al3x4ndra@users.noreply.github.com>
2024-09-07 00:22:38 -0300

    Upload de aplicação de pesquisa de filmes.

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        titulo: "Pânico",
+        descricao: "O enredo gira em torno de Sidney Prescott, uma estudante do ensino médio da cidade fictícia de Woodsboro, Califórnia, que se torna o alvo de um assassino misterioso conhecido como Ghostface.",
+        link:"https://pt.wikipedia.org/wiki/Scream_(1996)",
+        tags: "Wes Craven Williamson Slasher Ghostface"
+    },
+    {
+        titulo: "Entrevista Com o Vampiro",
+        descricao: " Um jornalista entrevista um jovem que afirma ser vampiro, narrando suas experiências dos últimos 200 anos. ",
+        link:"https://pt.wikipedia.org/wiki/Entrevista_com_o_Vampiro_(filme)",
+        tags: "Neil Jordan Gótico Vampiro"
+    },
+    {
+        titulo: "A Lenda do Cavaleiro sem Cabeça",
+        descricao: "Em 1799, uma série de crimes envolvendo inocentes acontece no pequeno vilarejo de Sleepy Hollow. Para investigar o caso é chamado o detetive nova-iorquino Ichabod Crane, um excêntrico e determinado oficial de polícia com um jeito avant-garde de solucionar crimes.",
+        link:"https://pt.wikipedia.org/wiki/A_Lenda_do_Cavaleiro_sem_Cabe%C3%A7a",
+        tags: "Tim Burton Jhonny Deep Suspence Fantasia"
+    },
+    {
+        titulo: "A Hora do Pesadelo | Pesadelo Final - A Morte de Freddy",
+        descricao: "Maggie Burroughs (Lisa Zane) é uma psicóloga que consegue penetrar nos pensamentos de John Doe (Shon Greenblatt), onde Freddy Krueger (Robert Englund) o ameaça. O jovem, foragido de Springwood, a cidade onde Freddy apareceu pela primeira vez, é o último sobrevivente jovem da região. Desmemoriado, ele afirma ser o filho de Freddy.",
+        link:"https://pt.wikipedia.org/wiki/Freddy%27s_Dead:_The_Final_Nightmare",
+        tags: "Rachel Talalay Freeddy Pesadelo"
+    },
+    {
+        titulo: "Um Drink no Inferno",
+        descricao: "Os irmãos Seth e Richard Gecko são procurados pela polícia por 16 mortes. Eles seqüestram um ex-pastor e seu casal de filhos, para poderem atravessar a fronteira com o México e lá se dirigem à uma casa noturna freqüentada por caminhoneiros e motoqueiros, que é uma mistura de cabaré e prostíbulo.",
+        link:"https://pt.wikipedia.org/wiki/From_Dusk_till_Dawn",
+        tags: "Robert Rodriguez Tarantino"
+    },
+    {
+        titulo: "Cemitério Maldito 2",
+        descricao: "Adolescente acaba ressuscitando a própria mãe, com consequências mortais.",
+        link:"https://pt.wikipedia.org/wiki/Pet_Sematary_II",
+        tags: "Mary Lambert "
+    },
+];
